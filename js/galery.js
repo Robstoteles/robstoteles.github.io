a24ae832164a2103e95ce969241597498286ea44
Robson Rocha <robson.rocha18@gmail.com>
2018-06-11 18:23:22 -0300

    Atualizando o site, criando uma galeria

--- js/galery.js
+++ js/galery.js
@@ -2,23 +2,23 @@
 //The code below create a slideshow in Vanilla JS
 var i = 0; 			// Start Point // Ponto Inicial
 var images = [];
-//var links  = [];
+var links  = [];
 var time = 4000;	// Time Between Switch // Tempo que cada imagem será exibida
 
 // definindo as imagens
 //Defining images
-images[0] = "img/slide1.jpg";
-images[1] = "img/slide2.jpg";
-images[2] = "img/slide6.jpg";
+images[0] = "img/jogo_corrida.png";
+//images[1] = "img/slide2.jpg";
+images[1] = "img/site_slide.png";
 //images[3] = "http://lorempixel.com/400/200/people";
 
 //Defining links
-/*links[0] = "car.html";
+links[0] = "car.html";
 //links[1] = "galery.html";
-links[1] = "fullscreen_slider.html"; */
+links[1] = "fullscreen_slider.html";
 
 //Getting 'a' tag attribute from the html file
-//var a = document.querySelector("a");
+var a = document.querySelector("a");
 // Change Image
 //Muda imagem
 function changeImg(){
@@ -26,7 +26,7 @@ function changeImg(){
 	//Defining the images from the array to the img tag
 	document.slider.src = images[i];
 	//Defining the url's from the array to the 'href' tag
-	//document.linker     = a.setAttribute("href", links[i]);
+	document.linker     = a.setAttribute("href", links[i]);
 
 	// Check If Index Is Under Max
 	//Verificando se o index é menor que o array images
